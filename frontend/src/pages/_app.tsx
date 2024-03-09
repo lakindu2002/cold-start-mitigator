73171d904ae17b6543d7a4c38fbf81c32373bbb2
lakindu hewawasam <lakindu2002@live.com>
2024-03-09 14:08:23 +0530

    adding mui

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,11 +2,16 @@ import { Amplify } from 'aws-amplify';
 import '@aws-amplify/ui-react/styles.css';
 import type { AppProps } from "next/app";
 import { amplifyConfig } from 'heatshield/config';
+import { ThemeProvider, createTheme } from '@mui/material';
 
 Amplify.configure(amplifyConfig);
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return <ThemeProvider
+    theme={createTheme()}
+  >
+    <Component {...pageProps} />
+  </ThemeProvider>;
 }
 
 export default App;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,4 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "heatshield/styles/Home.module.css";
 import { Header } from "heatshield/components/header";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
